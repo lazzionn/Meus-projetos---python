73e5337cde220970fff7c154c22bbde294c6a1c3
Lázaro Santos <lazarotoon@gmail.com>
2026-07-29 15:00:00 -0300

    Update jogo_da_forca.py
    Dei uma organizada no código, tirei algumas estruturas bem repetitivas, e deu uma enxutada no código

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -8,8 +8,6 @@ erros = 0
 acertos = 0
 jogadas = 0
 resposta_final = ''
-chute_certo = 0
-chute_errado = 0
 r2 = ''
 limite_erros = 8
 chute_certo = 0
@@ -40,18 +38,33 @@ base_palavras = [
     'biblioteca', 'academia', 'farmacia', 'padaria', 'aeroporto'
 ]
 
-def placar(a, e, j, cc, ce, tj): # a = acertos, e = erros, j = jogadas
-    os.system("clear")
-    a = (   print('--------------------------------------------------'),
-            print(f'|{21*' '}Placar{21*' '}|'),
-            print('--------------------------------------------------'),
-            print(f'|{3*' '}Acertos: {a}{5*' '}Erros: {e}{5*' '}Jogadas: {j}{7*' '}'),
-            print(f'|{3*' '}Chutes Certos: {cc}{5*' '}Chutes Errados: {ce}{7*' '}'),
-            print(f'|{3*' '}Tempo Jogado: {tj:.2f} minutos{19*' '}'),
-            print('--------------------------------------------------'),
-            print("Até mais! \nmade by: lazion") )
-    return a
+# FUNÇÕES PARA FACILITAR NA EXIBIÇÃO DE ESTRUTURAS REPETITIVAS
 
+def placar(a, e, j, cc, ce): # a = acertos, e = erros, j = jogadas
+    os.system("clear")
+    fim = time.perf_counter()
+    tempo_segundos = fim - inicio
+    minutos, segundos = divmod(int(tempo_segundos), 60)
+    print('--------------------------------------------------'),
+    print(f'|{21*' '}Placar{21*' '}|'),
+    print('--------------------------------------------------'),
+    print(f'|{3*' '}Acertos: {a}{5*' '}Erros: {e}{5*' '}Jogadas: {j}'),
+    print(f'|{3*' '}Chutes Certos: {cc}{5*' '}Chutes Errados: {ce}'),
+    print(f'|{3*' '}Tempo Jogado: {minutos}m {segundos}s'),
+    print('--------------------------------------------------'),
+    print("Até mais! \nmade by: lazion")
+
+def reiniciar_jogo(base_palavras):
+    print('-------------------------')
+    print("Palavra secreta definida!")
+    print('-------------------------')
+    resposta_final = ''
+    r2 = ''
+    erros = 0
+    acertos = 0
+    jogadas = 0
+    palavra_secreta = random.choice(base_palavras)
+    return resposta_final, r2, erros, acertos, jogadas, palavra_secreta
 
 palavra_secreta = random.choice(base_palavras)
 print('-------------------------')
@@ -73,24 +86,12 @@ while resposta_final != palavra_secreta:
         os.system('clear')
         chute_errado += 1
         print("Você errou a palavra secreta")
-        sair = input("Deseja recomeçar o jogo? [sim]/[nao] ").lower().startswith("s")
+        sair = input("Deseja continuar jogando? [sim]/[nao] ").lower().startswith("s")
         if sair is True:
-            print('-------------------------')
-            print("Palavra secreta definida!")
-            print('-------------------------')
-            
-            resposta_final = ''
-            r2 = ''
-            erros = 0
-            acertos = 0
-            jogadas = 0
-            palavra_secreta = random.choice(base_palavras)
+            resposta_final, r2, erros, acertos, jogadas, palavra_secreta = reiniciar_jogo(base_palavras)
             continue
         else:
-            fim = time.perf_counter()
-            tempo_segundos = fim - inicio
-            tempo_minutos = tempo_segundos / 60
-            placar(acertos_totais, erros_totais, jogadas_totais, chute_certo, chute_errado, tempo_minutos)
+            placar(acertos_totais, erros_totais, jogadas_totais, chute_certo, chute_errado)
             break
         
     if r1.isdigit():
@@ -100,34 +101,23 @@ while resposta_final != palavra_secreta:
         
 
     ################################
-    # Sistema de verificação das letras 
+    # Sistema de verificação das letras e palavras
     
     if len(r1) > 1 and r1 == palavra_secreta:
         os.system('clear')
         chute_certo += 1
-        print(f"Caramba! Você acertou a palavra secreta completa com \n{jogadas} jogadas e {erros} erro(s)!\nA palavra era {palavra_secreta.upper()}")
+        print("Caramba! Você acertou a palavra secreta completa!")
         sair = input("Deseja continuar jogando? [sim]/[nao] ").lower().startswith("s")
         if sair is True:
-            print('-------------------------')
-            print("Palavra secreta definida!")
-            print('-------------------------')
-            resposta_final = ''
-            r2 = ''
-            erros = 0
-            acertos = 0
-            jogadas = 0
-            palavra_secreta = random.choice(base_palavras)
+            resposta_final, r2, erros, acertos, jogadas, palavra_secreta = reiniciar_jogo(base_palavras)
             continue
         else:
-            fim = time.perf_counter()
-            tempo_segundos = fim - inicio
-            tempo_minutos = tempo_segundos / 60
-            placar(acertos, erros, jogadas, chute_certo, chute_errado, tempo_minutos)
+            placar(acertos, erros, jogadas, chute_certo, chute_errado)
             break
     else:
-        if r1 in palavra_secreta: 
+        if r1 in palavra_secreta:
             os.system('clear')
-            print(f"'{r1}' está na palavra secreta!") 
+            print(f"'{r1.upper()}' está na palavra secreta!")
             jogadas += 1
             acertos += 1
             jogadas_totais += 1
@@ -135,65 +125,43 @@ while resposta_final != palavra_secreta:
             print(f"Tentativas: {jogadas}")
             print(f"Erros: [{erros}/{limite_erros}]")
             
-            r2 += r1 
-            resposta_final = '' 
-            for letra in palavra_secreta: 
-                if letra in r2: 
-                    resposta_final += letra 
+            r2 += r1
+            resposta_final = ''
+            for letra in palavra_secreta:
+                if letra in r2:
+                    resposta_final += letra
                 else:
-                    resposta_final += '_ ' 
+                    resposta_final += '_ '
             print(f'Palavra formada: {resposta_final}')
             if resposta_final == palavra_secreta:
                 os.system('clear')
                 print(f"Parabens!  Você acertou a palavra secreta, com o total de {jogadas} jogadas, {erros} erros e {acertos} acertos!\n A palavra era {palavra_secreta.upper()}")
                 sair = input("Deseja continuar jogando? [sim]/[nao] ").lower().startswith("s")
                 if sair is True:
-                    print('-------------------------')
-                    print("Palavra secreta definida!")
-                    print('-------------------------')
-                    resposta_final = ''
-                    r2 = ''
-                    erros = 0
-                    acertos = 0
-                    jogadas = 0
-                    palavra_secreta = random.choice(base_palavras)
+                    resposta_final, r2, erros, acertos, jogadas, palavra_secreta = reiniciar_jogo(base_palavras)
                     continue
                 else:
-                    fim = time.perf_counter()
-                    tempo_segundos = fim - inicio
-                    tempo_minutos = tempo_segundos / 60
-                    placar(acertos, erros, jogadas, chute_certo, chute_errado, tempo_minutos)
+                    placar(acertos, erros, jogadas, chute_certo, chute_errado)
                     break
-        else: 
+        else:
             os.system('clear')
             print(f"'{r1}' não está na palavra secreta!")
-            print(f'Palavra formada: {resposta_final}')
             jogadas += 1
             erros += 1
             jogadas_totais += 1
             erros_totais += 1
             print(f"Tentativas: {jogadas}")
             print(f"Erros: [{erros}/{limite_erros}]")
+            print(f'Palavra formada: {resposta_final}')
             if erros > (limite_erros - 1):
                 os.system('clear')
                 print(f"Acabou suas tentativas! \nA palavra era {palavra_secreta.upper()}")
                 sair = input("Deseja continuar jogando? [sim]/[nao] ").lower().startswith("s")
                 if sair is True:
-                    print('-------------------------')
-                    print("Palavra secreta definida!")
-                    print('-------------------------')
-                    resposta_final = ''
-                    r2 = ''
-                    erros = 0
-                    acertos = 0
-                    jogadas = 0
-                    palavra_secreta = random.choice(base_palavras)
+                    resposta_final, r2, erros, acertos, jogadas, palavra_secreta = reiniciar_jogo(base_palavras)
                     continue
                 else:
-                    fim = time.perf_counter()
-                    tempo_segundos = fim - inicio
-                    tempo_minutos = tempo_segundos / 60
-                    placar(acertos, erros, jogadas, chute_certo, chute_errado, tempo_minutos)
+                    placar(acertos_totais, erros_totais, jogadas_totais, chute_certo, chute_errado)
                     break
 
 
